d0a57e63a4eb792da7b2bb084f69930e301e8710
Dylan <dquesada186@gmail.com>
2017-11-30 15:54:13 -0600

    dice function from Mike's leccture added, notes regarding potential draw function added.

---
+++ dice.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+//Deck of cards
+var hearts = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, K, Q];
+var spades = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, K, Q];
+var diamonds = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, K, Q];
+var clubs = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, K, Q];
+
+
+function draw(){
+	//random number from total number of cards left excluding the cards already in play.
+	//exentually will need an arguement that has the number to be applied to the random 
+	//number generator as well as the arrays with the available cards 
+}
+
+
+function rollDie(sides){
+	let output = Math.ceil(Math.random() * sides;
+	console.log(output);
+}
+
+
